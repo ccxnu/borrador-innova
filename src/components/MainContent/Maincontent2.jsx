7e1047caa630a110a0c91ba1997b69836c5f7a87
Leochrono <jamespaul1624@hotmail.com>
2024-06-03 21:48:39 -0500

    Avance 22

--- src/components/MainContent/Maincontent2.jsx
+++ src/components/MainContent/Maincontent2.jsx
@@ -6,229 +6,137 @@ const MainContent2 = () => {
     <Box>
       <Grid container spacing={6} >
         <Grid container justifyContent="center" >
-          <Typography
-            textAlign="center"
-            variant="body2"
-            fontSize="50px"
-            marginBottom="50px"
-            color="secondary"
-          >
-            <strong>Monetización:</strong>
-          </Typography>
-        </Grid>
-        <Grid container justifyContent="center">
-          <Typography
-            textAlign="center"
-            variant="body2"
-            fontSize="30px"
-            marginBottom="50px"
-          >
-            El cluster puede generar ingresos a través de:
-          </Typography>
-        </Grid>
-        <Grid item container xs={12} md={6} justifyContent="center">
           <Box
             sx={{
-              borderRadius: 2,
-              width: 350,
-              height: 230,
+              borderRadius: 0,
+              bgcolor: theme.palette.primary.main,
               textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Suscripciones Premium:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              Ofrece una membresía premium a las cooperativas que deseen acceso
-              exclusivo a contenido adicional, como webinars avanzados, informes
-              de tendencias y casos de estudio.
+              width: "100%",
+              maxWidth: "1400px",
+              padding: 1,
+            }}>
+            <Typography
+              textAlign="center"
+              variant="body1"
+              fontSize="26px"
+              color="secondary"
+            >
+              <strong>Monetización:</strong>
             </Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Marketplace de Soluciones Tecnológicas:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Crea un mercado en línea donde las cooperativas puedan comprar
-              soluciones tecnológicas específicas. 
-              b Obtén una comisión por cada venta realizada a través del marketplace.
-            </Typography>
-          </Box>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item container xs={12} md={5} direction="column" spacing={2}>
+            <Grid item>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Suscripciones Premium:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Ofrece una membresía premium a las cooperativas que deseen acceso exclusivo a contenido adicional, como webinars avanzados, informes de tendencias y casos de estudio.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Programas de Afiliados:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Establece acuerdos afiliados con proveedores tecnológicos. Gana comisiones por cada cliente referido que adquiera sus productos o servicios.
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+
+          <Grid item container xs={12} md={2} justifyContent="center">
+            <Box>
+              <img src="/static/Monetizacion.png" width="200px" height="200px" />
+            </Box>
+          </Grid>
+
+          <Grid item container xs={12} md={5} direction="column" spacing={2}>
+            <Grid item>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Consultoría Personalizada:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Ofrece servicios de consultoría estratégica y tecnológica a las cooperativas. Establece tarifas por hora o por proyecto.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Venta de Datos Agregados:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Anonimiza y agrupa datos relevantes. Venta de estos datos agregados a instituciones financieras o investigadores interesados.
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+
+          {/* Fila inferior con cuatro textos alineados horizontalmente */}
+          <Grid item container xs={12} spacing={2} justifyContent="space-between">
+            <Grid item xs={12} md={3}>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Alianzas Estratégicas con Fintech:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Colaborar con empresas Fintech para ofrecer soluciones conjuntas. Establecer acuerdos de ingresos compartidos basados en el éxito de la adopción de estas soluciones.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Eventos Patrocinados:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Organizar eventos (conferencias, talleres, hackathons) y busca patrocinadores. Las empresas tecnológicas y financieras pueden pagar por ser patrocinadores y tener visibilidad ante las cooperativas.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Programas de Certificación:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Organizar eventos (conferencias, talleres, hackathons) y busca patrocinadores. Las empresas tecnológicas y financieras pueden pagar por ser patrocinadores y tener visibilidad ante las cooperativas.
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+
+          {/* Fila final con los textos restantes alineados horizontalmente */}
+          <Grid item container xs={12} spacing={2} justifyContent="space-between">
+            <Grid item xs={12} md={3}>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Crowdfunding para Proyectos Innovadores:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Permite que las cooperativas presenten proyectos innovadores en la plataforma. Los miembros del cluster pueden invertir en estos proyectos a través de crowdfunding.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Box sx={{ width: "100%", height: "auto", textAlign: "left" }}>
+                <Typography variant="body1" fontSize="20px">
+                  <strong>Publicidad en la Plataforma Digital:</strong>
+                </Typography>
+                <Typography variant="body2" fontSize="18px">
+                  Ofrece espacios publicitarios en la plataforma. Las empresas interesadas pueden pagar por anuncios dirigidos a las cooperativas.
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item container xs={12} md={6} justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 2,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Consultoría Personalizada:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Ofrece servicios de consultoría estratégica y tecnológica a las
-              cooperativas. 
-              b Establece tarifas por hora o por proyecto.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Eventos Patrocinados:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Organiza eventos (conferencias, talleres, hackathons) y busca
-              patrocinadores. 
-              b Las empresas tecnológicas y financieras pueden pagar por ser patrocinadores y tener visibilidad ante las
-              cooperativas.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Alianzas Estratégicas con Fintechs:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Colabora con empresas fintech para ofrecer soluciones conjuntas.
-              b Establece acuerdos de ingresos compartidos basados en el éxito
-              de la adopción de estas soluciones.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Programas de Certificación:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Diseña programas de certificación en áreas como ciberseguridad,
-              gestión de datos o transformación digital. 
-              b Cobra tarifas por la participación en estos programas.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Venta de Datos Agregados:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Anonimiza y agrupa datos relevantes de las cooperativas. 
-              b Vende estos datos agregados a instituciones financieras o investigadores
-              interesados.patrocinadores y tener visibilidad ante las
-              cooperativas.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Crowdfunding para Proyectos Innovadores:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Permite que las cooperativas presenten proyectos innovadores en
-              la plataforma. 
-              b Los miembros del cluster pueden invertir en estos
-              proyectos a través de crowdfunding.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Programas de Afiliados:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Establece acuerdos de afiliados con proveedores de tecnología. 
-              b Gana comisiones por cada cliente referido que adquiera sus
-              productos o servicios.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6} container justifyContent="center">
-          <Box
-            sx={{
-              borderRadius: 1,
-              width: 350,
-              height: 230,
-              textAlign: "justify",
-              bgcolor: theme.palette.sixth.main,
-              marginBottom: "200px" 
-            }}
-          >
-            <Typography variant="body2" color="third" fontSize="20px">
-              <strong>Publicidad en la Plataforma Digital:</strong>
-            </Typography>
-            <Typography variant="body2" color="third" fontSize="20px">
-              a Ofrece espacios publicitarios en la plataforma. 
-              b Las empresas interesadas pueden pagar por anuncios dirigidos a las cooperativas
-            </Typography>
-          </Box>
-        </Grid>
+
       </Grid>
     </Box>
   );
